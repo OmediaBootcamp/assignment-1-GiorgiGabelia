bc075d1fe9678a9b396e97fb1bdbad5e055bd89e
GiorgiGabelia <giorgi.gabelia321@gmail.com>
2024-10-12 14:48:25 +0400

    solve DiagonalStar (exc27)

--- src/dev/omedia/section6/exc27/DiagonalStar.java
+++ src/dev/omedia/section6/exc27/DiagonalStar.java
@@ -1,14 +1,36 @@
 package dev.omedia.section6.exc27;
 
-// NOT FINISHED YET
-
 public class DiagonalStar {
-    public static void printSquareStar(int num) {
-        if(num < 5) {
+    private static final String STAR = "*";
+    private static final String SPACE = " ";
+
+    public static void main(String[] args) {
+        printSquareStar(9);
+    }
+
+    public static void printSquareStar(int number) {
+        if (number < 5) {
             System.out.println("Invalid Value");
             return;
         }
 
-
+        for (int row = 1; row <= number; row++) {
+            if (row == 1 || row == number) {
+                System.out.println(new String(new char[number]).replace("\0", STAR));
+                if (row == number) break;
+            } else {
+                System.out.print(STAR);
+                for (int col = 2; col < number; col++) {
+                    if ((col == row) || (number - col + 1 == row)) {
+                        System.out.print(STAR);
+                    } else {
+                        System.out.print(SPACE);
+                    }
+                }
+                System.out.println(STAR);
+            }
+        }
     }
+
 }
+
